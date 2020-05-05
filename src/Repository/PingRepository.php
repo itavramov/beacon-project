58fee7e97c3d7712b12b20b38b957422c921a52e
Ivan Avramov <itavramov@gmail.com>
2020-05-05 13:17:53 +0300

    reponsive as fuck

--- src/Repository/PingRepository.php
+++ src/Repository/PingRepository.php
@@ -9,7 +9,6 @@ use Doctrine\Persistence\ManagerRegistry;
 /**
  * @method Ping|null find($id, $lockMode = null, $lockVersion = null)
  * @method Ping|null findOneBy(array $criteria, array $orderBy = null)
- * @method Ping[]    findAll()
  * @method Ping[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class PingRepository extends ServiceEntityRepository
@@ -17,6 +16,11 @@ class PingRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Ping::class);
+    }
+
+    public function findAll()
+    {
+        return $this->findBy(array(), array('detectTime' => 'DESC'));
     }
 
     // /**
